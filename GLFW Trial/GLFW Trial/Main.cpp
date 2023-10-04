c826913d54180f89017ae2a726c1ee6493e66f9b
Krokoman13 <Bramtehaken@gmail.com>
2023-10-04 15:14:46 +0200

    Render a triangle by sending instructions to the GPU one at the time

--- GLFW Trial/GLFW Trial/Main.cpp
+++ GLFW Trial/GLFW Trial/Main.cpp
@@ -69,7 +69,16 @@ int main()
 
 		//==============
 		
-		// Draw commands go here
+		//start rendering triangles sending instructions
+		//to the GPU one at a time
+		glBegin(GL_TRIANGLES);
+			glColor3f(1, 0, 0);
+			glVertex3f(-0.5f, -0.5f, 0);
+			glColor3f(0, 1, 0);
+			glVertex3f(0.5f, -0.5f, 0);
+			glColor3f(0, 0, 1);
+			glVertex3f(-0.5f, 0.5f, 0);
+		glEnd();
 
 		//==============
 
